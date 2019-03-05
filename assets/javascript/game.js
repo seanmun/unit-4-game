dec625c3ae3fdaaecb3bec29fa2f9151aa022f0a
seanmun <smunley13@gmail.com>
2019-03-05 02:53:53 -0500

    d

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,63 +1,84 @@
 // Global vars
 var randomNumber;
-var losses;
-var wins;
-var images = ["assets/images/gem1", "assets/images/gem2", "assets/images/gem3", "assets/images/gem4", ];
+var losses = 0;
+var wins = 0;
+var images = ["assets/images/gem1.png", "assets/images/gem2.png", "assets/images/gem3.png", "assets/images/gem4.png"];
 var previous = 0;
-
+  
 //Start the game
 var startGame = function() {
 
-// Create random Number
-randomNumber = Math.floor(Math.random() * 101 + 19);
-console.log(randomNumber)
-$("#randomNumber").text(randomNumber)
+  $(".crystals").empty();
 
-// create random numer for crystals
-for (var i = 0; i < 4; i++) {
+  var images = [
+    'assets/images/gem1.png', 
+    'assets/images/gem2.png', 
+    'assets/images/gem3.png', 
+    'assets/images/gem4.png'
+  ]
+  // Create random Number
+  randomNumber = Math.floor(Math.random() * 101) + 19;
+  console.log(randomNumber);
+  $("#randomNumber").text(randomNumber);
 
-    var random = Math.floor(Math.random() * 12);
-    console.log(random)
-// Create crystam divs
-    var crystal = $("<div>");
-        crystal.attr({
-          "class": 'crystal',
-          "data-random": random
-        });
 
-    $(".crystals").append(crystal);
-}
+
+  // create random number for crystals
+  for (var i = 0; i < 4; i++) {
+
+      var random = Math.floor(Math.random() * 11) + 1;
+      console.log(random)
+  // Create crystal divs
+      var crystal = $("<div>");
+          crystal.attr({
+            "class": 'crystal',
+            "data-random": random,
+          });
+          crystal.css({
+            "background-image":"url('" + images[i] + "')",
+            "background-size": "cover"
+          })
+          crystal.html(random);
+
+      $(".crystals").append(crystal);
+  }
 }
 
 startGame()
 
+var reset = function(){
+
+}
 
 // on click for selecting crystals
-$(".crystal").on('click', function() {
+$(document).on('click', ".crystal", function() {
   // create a running count
   var count = parseInt($(this).attr('data-random'));
 
   // add count
   previous += count;
 
-  console.log(previous)
+  // print score
+  $("#finalTotal").text(previous);
+
+  console.log(count)
 
   // alert if win or lose
   if(previous > randomNumber) {
-   console.log("You Lose!");
    alert("You Lose!");
    //add losses to screen
-   losses++
-   $("#numberLoses").text(losses);
+   losses++;
+   $("#numberLosses").text(losses);
+   previous = 0;
    startGame();
   }
   else if(previous === randomNumber){
-    console.log("You did it!");
     alert("You Win!");
     //add wins to screen
-    wins++
-    $("#numberWins").text(wins)
-    startGame()
+    wins++;
+    $("#numberWins").text(wins);
+    previous = 0;
+    startGame();
   }
 })
 
